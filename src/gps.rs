fad9a0484fbd5ca79e13a6804ce5109de0036916
weiss <kingbaiing@163.com>
2022-12-26 17:57:51 +0100

    export gps geometry containers

--- src/gps.rs
+++ src/gps.rs
@@ -1,5 +1,13 @@
 use crate::types;
 
+pub type MultiPointC<Pt> = types::MultiPoint<4326, Pt>;
+pub type LineStringC<Pt> = types::LineString<4326, Pt>;
+pub type MultiLineStringC<Pt> = types::MultiLineString<4326, Pt>;
+pub type PolygonC<Pt> = types::Polygon<4326, Pt>;
+pub type MultiPolygonC<Pt> = types::MultiPolygon<4326, Pt>;
+pub type GeometryContainerC<Pt> = types::GeometryContainer<4326, Pt>;
+pub type GeometryCollectionC<Pt> = types::GeometryCollection<4326, Pt>;
+
 pub type Point = types::Point<4326>;
 pub type MultiPoint = types::MultiPoint<4326, Point>;
 pub type LineString = types::LineString<4326, Point>;
